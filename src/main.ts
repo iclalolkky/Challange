eacf665a26ad12d6b64d917427b9680bce27fa94
İclal Olukkaya <iclalolukkaya67@gmail.com>
2026-03-04 09:28:55 +0300

    feat: add input component with a11y support

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,33 @@
 import './styles/main.scss';
 import { createButton } from './components/Button/Button';
+import { createInput } from './components/Input/Input';
 
 // HTML #app div
 const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app) {
-  // 1. Primary Buton 
+  // Primary Buton 
   const primaryBtn = createButton({
     text: 'Hemen Başla',
     variant: 'primary',
     onClick: () => alert('Primary butona tıklandı!')
   });
 
-  // 2. Outline Buton 
+  // Outline Buton 
   const outlineBtn = createButton({
     text: 'Daha Fazla Bilgi',
     variant: 'outline',
   });
 
+  const emailInput = createInput({
+    label: 'E-posta Adresiniz',
+    id: 'email-input',
+    type: 'email',
+    placeholder: 'ornek@sirket.com',
+    required: true
+  });
+
   app.appendChild(primaryBtn);
   app.appendChild(outlineBtn);
+  app.appendChild(emailInput);
 }
